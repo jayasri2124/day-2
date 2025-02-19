c0f4e73099de787997de671323e7a1b80e0c779d
jayasri2124 <jayasri22td0682@svcet.ac.in>
2025-02-19 08:27:33 -0800

    Updated

--- code_2.py
+++ code_2.py
@@ -1,12 +1,14 @@
-def add(a,b):
-    return a+b
-    
-def sub(a,b):
-    return a - b
-def mul(a,b):
-    return  a * b
+"""list=[1,6,3,8,9]
+list.sort()
+print(list)
 
-    
+list=[4,1,8,3,6]
+x=list.count(2)
+print(x)"""
+
+list=["A",-20,"a",94,66]
+x=sorted(list,key=str)
+print(x)
 
 
 
